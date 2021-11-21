d3b5846a12016f0550e65e11f2ed8f493927d1dc
kylehassall27 <kylehassall27@gmail.com>
2021-11-20 21:22:47 -0500

    Got multicast working, added Tweet types

---
+++ AtlasIDE/Networking.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Numerics;
+using System.Text;
+using System.Threading;
+using Newtonsoft.Json;
+
+namespace AtlasIDE
+{
+    public class Networking
+    {
+        private static UdpClient udpClient;
+        public static void Start()
+        {
+            udpClient = new UdpClient(1235);
+            udpClient.JoinMulticastGroup(IPAddress.Parse("232.1.1.1"), 16);
+
+            var receiveThread = new Thread(Receive);
+            receiveThread.Start();
+        }
+
+        public static void Receive()
+        {
+            while (true)
+            {
+                var ipEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                var data = udpClient.Receive(ref ipEndPoint);
+
+                var Message = Encoding.Default.GetString(data);
+
+                var result = JsonConvert.DeserializeObject<Tweet>(Message);
+                switch (result.TweetType) {
+                    case "Identity_Thing":
+                        result = JsonConvert.DeserializeObject<IdentityThingTweet>(Message);
+                        break;
+                    case "Identity_Language":
+                        result = JsonConvert.DeserializeObject<IdentityLanguageTweet>(Message);
+                        break;
+                    case "Identity_Entity":
+                        result = JsonConvert.DeserializeObject<IdentityEntityTweet>(Message);
+                        break;
+                    case "Service":
+                        result = JsonConvert.DeserializeObject<ServiceTweet>(Message);
+                        break;
+
+                    default:
+                        break;
+                }
+
+                Console.WriteLine(Message);
+            }
+        }
+    }
+}
---
+++ AtlasIDE/Tweet.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtlasIDE
+{
+    public class Tweet
+    {
+        [JsonProperty(PropertyName = "Tweet Type")]
+        public string TweetType { get; set; }
+        [JsonProperty(PropertyName = "Thing ID")]
+        public string ThingID { get; set; }
+        [JsonProperty(PropertyName = "Space ID")]
+        public string SpaceID { get; set; }
+    }
+
+
+    public class IdentityThingTweet : Tweet
+    {
+        [JsonProperty(PropertyName = "Name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "Model")]
+        public string Model { get; set; }
+        [JsonProperty(PropertyName = "Vendor")]
+        public string Vendor { get; set; }
+        [JsonProperty(PropertyName = "Owner")]
+        public string Owner { get; set; }
+        [JsonProperty(PropertyName = "Description")]
+        public string Description { get; set; }
+        [JsonProperty(PropertyName = "OS")]
+        public string OS { get; set; }
+    }
+
+
+    public class IdentityLanguageTweet : Tweet
+    {
+        [JsonProperty(PropertyName = "Network Name")]
+        public string NetworkName { get; set; }
+        [JsonProperty(PropertyName = "Communication Language")]
+        public string CommunicationLanguage { get; set; }
+        [JsonProperty(PropertyName = "IP")]
+        public string IP { get; set; }
+        [JsonProperty(PropertyName = "Port")]
+        public string Port { get; set; }
+    }
+
+    public class IdentityEntityTweet : Tweet
+    {
+        [JsonProperty(PropertyName = "Name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "ID")]
+        public string ID { get; set; }
+        [JsonProperty(PropertyName = "Type")]
+        public string Type { get; set; }
+        [JsonProperty(PropertyName = "Owner")]
+        public string Owner { get; set; }
+        [JsonProperty(PropertyName = "Vendor")]
+        public string Vendor { get; set; }
+        [JsonProperty(PropertyName = "Description")]
+        public string Description { get; set; }
+    }
+
+
+    public class ServiceTweet : Tweet
+    {
+        [JsonProperty(PropertyName = "Name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "Entity ID")]
+        public string EntityID { get; set; }
+        [JsonProperty(PropertyName = "Vendor")]
+        public string Vendor { get; set; }
+        [JsonProperty(PropertyName = "API")]
+        public string API { get; set; }
+        [JsonProperty(PropertyName = "Type")]
+        public string Type { get; set; }
+        [JsonProperty(PropertyName = "AppCategory")]
+        public string AppCategory { get; set; }
+        [JsonProperty(PropertyName = "Description")]
+        public string Description { get; set; }
+        [JsonProperty(PropertyName = "Keywords")]
+        public string Keywords { get; set; }
+    }
+
+}
