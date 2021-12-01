0cd55cbe49ae4bfc29c745bb508b3a1ba7107f73
jmho <justinho@ufl.edu>
2021-12-01 16:51:09 -0500

    Merged kyle and justin branch

--- AtlasIDE/MainWindow.xaml.cs
+++ AtlasIDE/MainWindow.xaml.cs
@@ -7,14 +7,28 @@ namespace AtlasIDE
     /// </summary>
     public partial class MainWindow : Window
     {
+<<<<<<< Updated upstream
         
         Relationship rel;
         bool initRel = false;
         System.Windows.Controls.ListBox dragSource = null; 
+=======
+        public ICollectionView view;
+        Relationship rel;
+        bool initRel = false;
+        System.Windows.Controls.ListBox dragSource = null;
+>>>>>>> Stashed changes
         public MainWindow()
         {
             InitializeComponent();
             Networking.Start();
+<<<<<<< Updated upstream
+=======
+            
+            ShowRelEdit(false);
+            // Remove when done
+
+>>>>>>> Stashed changes
             RelationshipTweet tweet = new RelationshipTweet();
             rel = new Relationship(tweet);
             rel.Name = "Test Relationship";
@@ -27,12 +41,21 @@ namespace AtlasIDE
             rel.ThingID = "6969";
             rel.Type = "Software";
 
+<<<<<<< Updated upstream
             lbDrop.AllowDrop = true;
 
             lbRelationship.Items.Add(rel.Name);
             ShowRelEdit(false);
         }
 
+=======
+            lbRelationship.Items.Add(rel.Name);
+
+
+        }
+
+        // Justin Relationship Code
+>>>>>>> Stashed changes
         private void btRelEdit_Click(object sender, RoutedEventArgs e)
         {
             ShowRelEdit(true);
@@ -45,7 +68,11 @@ namespace AtlasIDE
 
             if (show) // Show Relationship edit form
             {
+<<<<<<< Updated upstream
                 if(lbRelationship.SelectedItem == null)
+=======
+                if (lbRelationship.SelectedItem == null)
+>>>>>>> Stashed changes
                 {
                     MessageBox.Show("Error you must select a relationship to edit!");
                     return;
@@ -98,6 +125,7 @@ namespace AtlasIDE
 
             }
 
+<<<<<<< Updated upstream
         }
 
         private void btRelSave_Click(object sender, RoutedEventArgs e)
@@ -109,6 +137,72 @@ namespace AtlasIDE
 
 
         private void DragOver_Recipe(object sender, DragEventArgs e)
+=======
+        }
+
+        private void btRelSave_Click(object sender, RoutedEventArgs e)
+        {
+            ShowRelEdit(false);
+            MessageBox.Show("Relationship saved!");
+        }
+
+        // Source for drag and drop between listboxes -> https://www.c-sharpcorner.com/uploadfile/dpatra/drag-and-drop-item-in-listbox-in-wpf/
+        private void lbRelationship_PreMouseLeftButton(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            System.Windows.Controls.ListBox parent = (System.Windows.Controls.ListBox)sender;
+            dragSource = parent;
+            object data = GetDataFromListBox(dragSource, e.GetPosition(parent));
+
+            if (data != null)
+            {
+                DragDrop.DoDragDrop(parent, data, DragDropEffects.Move);
+            }
+
+        }
+        private static object GetDataFromListBox(System.Windows.Controls.ListBox source, Point point)
+        {
+            UIElement element = source.InputHitTest(point) as UIElement;
+            if (element != null)
+            {
+                object data = DependencyProperty.UnsetValue;
+                while (data == DependencyProperty.UnsetValue)
+                {
+                    data = source.ItemContainerGenerator.ItemFromContainer(element);
+
+                    if (data == DependencyProperty.UnsetValue)
+                    {
+                        element = System.Windows.Media.VisualTreeHelper.GetParent(element) as UIElement;
+                    }
+
+                    if (element == source)
+                    {
+                        return null;
+                    }
+                }
+
+                if (data != DependencyProperty.UnsetValue)
+                {
+                    return data;
+                }
+            }
+
+            return null;
+        }
+
+        private void lbDrop_Drop(object sender, DragEventArgs e)
+        {
+            System.Windows.Controls.ListBox parent = (System.Windows.Controls.ListBox)sender;
+            object data = e.Data.GetData(typeof(string));
+            //dragSource.Items.Remove(data);
+
+            parent.Items.Add(data);
+
+        }
+
+        // Kyle Thing/Service Code
+
+        public void UpdateThings()
+>>>>>>> Stashed changes
         {
 
            // TBD
@@ -130,7 +224,17 @@ namespace AtlasIDE
 
         }
 
+<<<<<<< Updated upstream
         private static object GetDataFromListBox(System.Windows.Controls.ListBox source, Point point)
+=======
+        public void UpdateRelationships()
+        {
+            lbRelationship.ItemsSource = null;
+            lbRelationship.ItemsSource = view;
+        }
+
+        public void Filter()
+>>>>>>> Stashed changes
         {
             UIElement element = source.InputHitTest(point) as UIElement;
             if (element != null)
@@ -170,6 +274,16 @@ namespace AtlasIDE
 
             parent.Items.Add(data);
             
+        }
+
+        private void recipe_DragOver(object sender, DragEventArgs e)
+        {
+            Dispatcher.BeginInvoke((Action)(() => tabControl.SelectedIndex = 3));
+        }
+
+        private void Relationships_DragOver(object sender, DragEventArgs e)
+        {
+            Dispatcher.BeginInvoke((Action)(() => tabControl.SelectedIndex = 2));
         }
     }
 }
